d39e6c3952a9dc62dff2ca874ef8e0a80a77b6d3
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2023-03-06 10:43:12 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,21 @@
 #! python3
 
 # SD Computing Studies Assignment
+
+import random
+
+def rand1(max):
+    random1 = random.randint(1,100)
+    guess = ""
+    random1 = int(random1)
+    guess = int(guess)
+    while guess not in random1:
+        print("guess the number!")
+    guess = input("guess the number!")
+    if guess > random1:
+        print("that number is lower than the random one\n")
+    if guess < random1:
+        print("that number is more than the random one\n")
+    if guess == random1:
+        print("you got it! that was the number")
+    
