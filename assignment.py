a23f76245e381416667e8a6f546b61a8557ca4c9
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2023-03-07 10:01:40 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -10,15 +10,15 @@ def rand1(max):
     random1 = float(random1)
     guess = input("guess the number: ")
     guess = float(guess)
-    total = 1
+    total = 0
     total = int(total)
     def repeat():
         guess = input("guess the number: ")
         guess = float(guess)
         if guess > random1:
-            print("that number is more than the random one\n") and total + 1
+            print("that number is more than the random one\n") and total += int(total + 1)
         if guess < random1:
-            print("that number is lower than the random one\n") and total + 1
+            print("that number is lower than the random one\n") and total += int(total + 1)
         if guess == random1:
             print(f"you got it! that was the number! your total guesses was: {total}")
             return False
@@ -26,13 +26,13 @@ def rand1(max):
             print("guess again!")
         return True
     if guess > random1:
-        print("that number is more than the random one\n") and total + 1
+        print("that number is more than the random one\n") and total += int(total + 1)
     if guess < random1:
-        print("that number is lower than the random one\n") and total + 1
+        print("that number is lower than the random one\n") and total += int(total + 1)
     if guess == random1:
-        print("you got it! that was the number")
+        print("you got it! that was the number! your total guesses was: {total}")
     if guess != random1 or guess == 0:
-        print("guess again!") and total + 1
+        print("guess again!") and total += int(total + 1)
     
     while True:
         if repeat() == False:
